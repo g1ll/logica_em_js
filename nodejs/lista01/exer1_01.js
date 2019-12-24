db5bdeb1c66a1c9caf05e8570f21767c44ea52ec
g1ll <gillvelleda@gmail.com>
2019-12-24 00:17:25 -0300

    Teste exemplo 1

--- nodejs/lista01/exer1_01.js
+++ nodejs/lista01/exer1_01.js
@@ -5,3 +5,21 @@ const print = console.info
 print("Informe um valor para A:");
 var a = scanf("%d")
 print("Valor de a é: %d",a);
+
+print("Informe um valor para B:");
+var b = scanf("%d")
+
+print("Informe um valor para C:");
+var c = scanf("%d")
+
+print("\nValores Informados: \n\tA = %d\n\tB = %d\n\tC = %d\n",a,b,c);
+
+var	area_tri = a*b/2;
+var	area_qua = b*b;
+var	area_ret = area_tri*2;
+var	area_cir = Math.PI*c**2;
+
+print("\na) Área do triângulo: %.2f\n",area_tri);
+print("\nb) Área do quadrado: %.2f\n",area_qua);
+print("\nc) Área do retângulo: %.2f\n",area_ret);
+print("\nd) Área do triângulo: %f\n\n",area_cir);
