b76328db5c5f9cde1a680c507e6b8c9394964ea7
g1ll <gillvelleda@gmail.com>
2019-12-23 23:49:54 -0300

    Exercício 1 da Lista 1 - Introdução a algoritmos

---
+++ nodejs/lista01/exer1_01.js
@@ -0,0 +1,7 @@
+/*JS*/
+const scanf = require( 'scanf');//Instalar com 'npm install ler --save' na raiz do seu projeto
+const print = console.info
+
+print("Informe um valor para A:");
+var a = scanf("%d")
+print("Valor de a é: %d",a);
